d9b0df52161e296b234bd05380b819333924677e
Yanan Li <luulsj@163.com>
2021-06-01 11:49:51 +1200

    add deploy command

--- src/app/pipe/format-date.pipe.ts
+++ src/app/pipe/format-date.pipe.ts
@@ -7,7 +7,7 @@ export class FormatDatePipe implements PipeTransform {
 
   transform(value: number, ...args: unknown[]): string {
     let date = new Date(value)
-    return date.getFullYear() + '/' + date.getMonth() + '/' + date.getDate() + ' ' + date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds();
+    return date.getFullYear() + '/' + (date.getMonth() + 1) + '/' + date.getDate() + ' ' + date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds();
   }
 
 }
